492a2fd689901343e3c12f41ef20adc0c142262d
Cillian Myles <cillian.myles@datavant.com>
2026-03-31 13:32:44 +0100

    feat: simple hard coded prompt

--- main.py
+++ main.py
@@ -1,5 +1,24 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+
+
 def main():
-    print("Hello from python-agent!")
+    load_dotenv()
+    gemini_api_key = os.environ.get("GEMINI_API_KEY")
+    if not gemini_api_key:
+        raise RuntimeError(
+            'Required environment variable "GEMINI_API_KEY" was missing or empty!'
+        )
+    print(f'Loaded environment variable... "GEMINI_API_KEY" --> "{gemini_api_key}"')
+
+    client = genai.Client(api_key=gemini_api_key)
+    model = "gemini-2.5-flash"
+    prompt = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+    response = client.models.generate_content(model=model, contents=prompt)
+    print(f'Model --> "{model}"')
+    print(f'Prompt --> "{prompt}"')
+    print(f'Response --> "{response.text}"')
 
 
 if __name__ == "__main__":

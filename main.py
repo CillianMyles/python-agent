799be618ec92e5fa3df9dc7d22ec1d1d72c54f76
Cillian Myles <myles.cillian@gmail.com>
2026-03-31 20:43:02 +0100

    feat: print tokens involved in prompt and response

--- main.py
+++ main.py
@@ -3,22 +3,35 @@ from dotenv import load_dotenv
 from google import genai
 
 
+MODEL = "gemini-2.5-flash"
+PROMPT = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+
+
 def main():
     load_dotenv()
-    gemini_api_key = os.environ.get("GEMINI_API_KEY")
+    gemini_api_key = os.environ.get("GEMINI_API_KEY", "")
     if not gemini_api_key:
         raise RuntimeError(
             'Required environment variable "GEMINI_API_KEY" was missing or empty!'
         )
-    print(f'Loaded environment variable... "GEMINI_API_KEY" --> "{gemini_api_key}"')
+
+    print(f'GEMINI_API_KEY: "{gemini_api_key}"')
+    print(f'Model: "{MODEL}"')
+    print(f'Prompt: "{PROMPT}"')
 
     client = genai.Client(api_key=gemini_api_key)
-    model = "gemini-2.5-flash"
-    prompt = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
-    print(f'Model --> "{model}"')
-    print(f'Prompt --> "{prompt}"')
-    response = client.models.generate_content(model=model, contents=prompt)
-    print(f'Response --> "{response.text}"')
+    response = client.models.generate_content(model=MODEL, contents=PROMPT)
+
+    if not response:
+        raise RuntimeError("expected response")
+
+    print(f'Response: "{response.text}"')
+
+    if not response.usage_metadata:
+        raise RuntimeError("expected response usage metadata")
+
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
 
 if __name__ == "__main__":

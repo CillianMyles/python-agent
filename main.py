1b2ce7b91a5dba3418c83bce8c7992f4357901c6
Cillian Myles <myles.cillian@gmail.com>
2026-04-15 22:50:03 +0100

    chore: tidy up

--- main.py
+++ main.py
@@ -14,22 +14,18 @@ def main():
         raise RuntimeError(
             'Required environment variable "GEMINI_API_KEY" was missing or empty!'
         )
-
     print(f'GEMINI_API_KEY: "{gemini_api_key}"')
     print(f'Model: "{MODEL}"')
     print(f'Prompt: "{PROMPT}"')
 
     client = genai.Client(api_key=gemini_api_key)
     response = client.models.generate_content(model=MODEL, contents=PROMPT)
-
     if not response:
         raise RuntimeError("expected response")
-
     print(f'Response: "{response.text}"')
 
     if not response.usage_metadata:
         raise RuntimeError("expected response usage metadata")
-
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
     print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 

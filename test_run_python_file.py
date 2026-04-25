f12b51173ca9f59baa8c634b3a5aaf6a67b45211
Cillian Myles <cillian.myles@datavant.com>
2026-04-25 19:37:52 +0100

    feat: function calling: run python file (WIP)

--- test_run_python_file.py
+++ test_run_python_file.py
@@ -2,27 +2,27 @@ from functions.run_python_file import run_python_file
 
 
 def main():
-    print("main.py")
+    print("main.py:")
     print(run_python_file("calculator", "main.py"))
     print("")
 
-    print('main.py "3 + 5"')
+    print('main.py "3 + 5":')
     print(run_python_file("calculator", "main.py", ["3 + 5"]))
     print("")
 
-    print("tests.py")
+    print("tests.py:")
     print(run_python_file("calculator", "tests"))
     print("")
 
-    print("../main.py")
+    print("../main.py:")
     print(run_python_file("calculator", "../main.py"))
     print("")
 
-    print("nonexistent.py")
+    print("nonexistent.py:")
     print(run_python_file("calculator", "nonexistent.py"))
     print("")
 
-    print("lorem.txt")
+    print("lorem.txt:")
     print(run_python_file("calculator", "lorem.txt"))
     print("")
 
